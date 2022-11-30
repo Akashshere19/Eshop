6c0704e083b07a49746e8ed145d8c56b481eb947
akashshere19 <akashshere19@gmail.com>
2022-11-30 20:54:45 +0530

    initial commit

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models.product import Product
 from .models.category import Category
+from .models.customer import Customer
 # Register your models here.
 
 class AdminProduct(admin.ModelAdmin):
@@ -8,7 +9,9 @@ class AdminProduct(admin.ModelAdmin):
 
 class AdminCategory(admin.ModelAdmin):
     list_display = ['name']
-
+class AdminCustomer(admin.ModelAdmin):
+    list_display = ['first_name','last_name','phone','email','password']
     
 admin.site.register(Category,AdminCategory)
 admin.site.register(Product,AdminProduct)
+admin.site.register(Customer,AdminCustomer)
--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models.product import Product
 from .models.category import Category
+from .models.customer import Customer
 # Create your views here.
 def index(request):
     prds  = None
@@ -23,4 +24,34 @@ def index(request):
 
   
 def signup(request): 
-    return render(request,'signup.html')
+    if request.method == 'GET':
+        print(request.method) # showing types of request 
+        return render(request,'signup.html')
+       
+    else:
+        postData = request.POST
+        first_name = postData.get('first_Name')
+        last_name = postData.get('last_Name')
+        phone = postData.get('phone')
+        email = postData.get('email')
+        password = postData.get('password')
+
+        error_msg = None
+        if (not first_name):
+            error_msg = 'first Name is Requiered..!'
+        elif first_name:
+            if len(first_name) < 4:
+                error_msg = 'first Name must be 4 character..!'
+        elif not phone:
+            error_msg = "phone no must..!"
+
+        # print(first_name,last_name,phone,email,password)
+        if not error_msg :
+                     customer = Customer(first_name=first_name,last_name=last_name,
+                                phone=phone,email=email,
+                                password=password)
+                     customer.register()
+                     return HttpResponse('account Create')
+        else:
+
+                return render(request,'signup.html',{'error':error_msg})
